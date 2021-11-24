73109677ab7f4a08f05714a74a972def8b27d298
AndresK22 <hg19010@ues.edu.sv>
2021-11-24 17:54:12 -0600

    Funcion de agregar videojuegos lista

--- appGrupo13/js/main.js
+++ appGrupo13/js/main.js
@@ -12,3 +12,26 @@ function deleteVideojuego(){
 		location.reload();
 	}
 }
+
+//Funcion para agregar videojuegos a la API
+function addVideojuego(){
+	var videojuegoadd={titulo:document.getElementById("titulo").value,
+	autor:document.getElementById("autor").value,	
+	imagen:document.getElementById("imagen").value,
+	categoria:document.getElementById("categoria").value,
+	descripcion:document.getElementById("descripcion").value}
+
+	var addresult;
+	fetch("http://127.0.0.1:3000/videojuegos",
+	{
+		method:"POST",
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(videojuegoadd),
+	})
+	.then(response=>response.json())
+	.then(data=>addresult=data);
+	addresult;
+	location.reload();
+}
