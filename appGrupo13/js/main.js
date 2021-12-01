133cf95d64ca7434e7cfd20e99adca4327843e4a
Josue Ernesto Cruz Cuellar <cc19114@ues.edu.sv>
2021-11-30 20:34:02 -0600

    Listar videojuegos

--- appGrupo13/js/main.js
+++ appGrupo13/js/main.js
@@ -1,3 +1,10 @@
+var fila="<tr><td class='id'></td><td class='imagen'></td><td class='titulo'></td><td class='autor'></td><td class='categoria'></td><td class='descripcion'></td><td class='action'></td></tr>";
+
+function crearBoton(id){
+	var boton="<button class='btn btn-danger' onclick='deleteVideojuego("+id+");'>Borrar</button>";
+	return boton;
+}
+
 //Funcion para eliminar videojuegos de la API
 function deleteVideojuego(){
 	var idVideojuego = document.getElementById("deleteid").value;
@@ -35,3 +42,87 @@ function addVideojuego(){
 	addresult;
 	location.reload();
 }
+//Funcion para obtener videojuegos
+
+function obtenerVideojuegos() {
+	fetch('http://localhost:3000/videojuegos')
+		  .then(res=>res.json())
+		  .then(data=>{
+			  videojuegos=data;
+			  listaVideojuegos(data)})
+		  .catch(error => {
+				  alert(error);
+			  })
+}
+
+
+function codigoCat(catstr) {
+  var code="null";
+  switch(catstr) {
+	  case "accion":code="c1";break;
+	  case "estrategia":code="c2";break;
+	  case "deporte":code="c3";break;
+	  case "carreras":code="c4";break;
+  }
+  return code;
+}   
+	var orden=0;
+	
+  function listaVideojuegos(videojuegos) {
+	var t=document.getElementById("name");
+	t.setAttribute("onclick", "orden*=-1;listaVideojuegos(videojuegos);");
+	var num = videojuegos.length;
+	var listado=document.getElementById("listado");
+	var ids,titles,autor,images,categories,description,actions;
+	var tbody=document.getElementById("tbody"),nfila=0;
+	tbody.innerHTML="";
+	var catcode;
+	for(i=0;i<num;i++) tbody.innerHTML+=fila;
+	var tr; 
+	ids=document.getElementsByClassName("id");
+	titles=document.getElementsByClassName("titulo");
+	autor=document.getElementsByClassName("autor");
+	categories=document.getElementsByClassName("categoria");   
+	images=document.getElementsByClassName("imagen");   
+	description=document.getElementsByClassName("descripcion"); 
+	actions=document.getElementsByClassName("action"); 
+	if(orden===0) {orden=-1;t.innerHTML="Nombre"}
+	else
+	   if(orden==1) {ordenarAsc(videojuegos,'titulo');t.innerHTML="Nombre A";t.style.color="darkgreen"}
+	   else 
+		 if(orden==-1) {ordenarDesc(videojuegos,'titulo');t.innerHTML="Nombre D";t.style.color="blue"}
+	  
+		var boton="";
+		  listado.style.display="block";
+	for(nfila=0;nfila<num;nfila++) {
+		  ids[nfila].innerHTML=videojuegos[nfila].id;
+		  boton = crearBoton(videojuegos[nfila].id);
+		  titles[nfila].innerHTML=videojuegos[nfila].titulo;
+		  autor[nfila].innerHTML=videojuegos[nfila].autor;
+		  categories[nfila].innerHTML=videojuegos[nfila].categoria;
+		  catcode=codigoCat(videojuegos[nfila].categoria);
+		  description[nfila].innerHTML=videojuegos[nfila].descripcion;
+		  tr=categories[nfila].parentElement;
+		  tr.setAttribute("class",catcode);
+		  images[nfila].innerHTML="<img src='"+videojuegos[nfila].imagen+"'>";
+		  images[nfila].firstChild.setAttribute("onclick","window.open('"+videojuegos[nfila].imagen+"');" );
+		  actions[nfila].innerHTML= boton;
+
+	  }
+  }
+
+  function ordenarDesc(p_array_json, p_key) {
+	p_array_json.sort(function (a, b) {
+	   if(a[p_key] > b[p_key]) return -1;
+ if(a[p_key] < b[p_key]) return 1;
+ return 0;
+	});
+ }
+ 
+ function ordenarAsc(p_array_json, p_key) {
+	p_array_json.sort(function (a, b) {
+	   if(a[p_key] > b[p_key]) return 1;
+ if(a[p_key] < b[p_key]) return -1;
+ return 0;
+	});
+ }
