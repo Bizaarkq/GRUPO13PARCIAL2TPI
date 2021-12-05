8aa28af37d423c73e96a665b8f2086e6d664535c
Fatimmaag7 <fatimmaaguilar7@gmail.com>
2021-12-05 14:58:31 -0600

    Agregando css

--- appGrupo13/js/main.js
+++ appGrupo13/js/main.js
@@ -4,12 +4,12 @@ var idmod = null;
 var videojuegos = null;
 
 function crearBoton(id){
-	var boton="<button class='btn btn-danger' type='button' onclick='deleteVideojuego("+id+");'>Borrar</button>";
+	var boton="<button class='btn btn-danger' type='button' onclick='deleteVideojuego("+id+");'><i class='far fa-trash-alt'></i></button>";
 	return boton;
 }
 
 function crearBotonModificar(id){
-	var boton="<button type='button' class='btn btn-primary' onclick='moverdatosaform("+id+")'>Editar</button>"
+	var boton="<button type='button' class='btn btn-primary' onclick='moverdatosaform("+id+")'><i class='fas fa-pen'></i></button>"
 	//var boton="<button class='btn btn-primary' onclick='actualizarVideojuego("+id+");'>Modificar</button>";
 	return boton;
 }
@@ -53,7 +53,8 @@ function actualizarVideojuego(){
 			'Accept': 'application/json',
 			'Content-type': 'application/json; charset=UTF-8',
 		}}).then(response=>response.json()).then(data=>addresult=data);
-		alert("Se edito el videojuego con ID #"+codigoJuego);
+		swal("Editar", "Se edito el videojuego con ID #"+codigoJuego, "success");
+	
 		
 		idmod=null;
 		document.getElementById("titulo").value = '';
